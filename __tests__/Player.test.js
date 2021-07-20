5108910f61bd2521a1272717c90e50103b136923
nicopetelo <nicopetelo@gmail.com>
2021-07-19 20:01:08 -0700

    back in it

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -14,5 +14,22 @@ test('creates a player object', () => {
     expect(player.inventory).toEqual(
         expect.arrayContaining([expect.any(Object)])
     );
+    
 
+
+});
+
+test('gets player stats as an object', () => {
+    const player = new Player('Gordon');
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+});
+
+test('gets inventory from a player or returns false', () => {
+    const player = new Player('Gordon');
+    expect(player.getInventory()).toEqual(expect.any(Array));
+    player.inventory = [];
+    expect(player.getInventory()).toEqual(false);
 });
